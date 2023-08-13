65789c470125420cdd6306a84b0944041094daf0
Aman <akapur07@gmail.com>
2023-08-14 01:46:51 +0530

    Employee GET

--- employee/src/main/java/com/kubernetesmicroservices/employee/EmployeeApplication.java
+++ employee/src/main/java/com/kubernetesmicroservices/employee/EmployeeApplication.java
@@ -1,7 +1,15 @@
 package com.kubernetesmicroservices.employee;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @SpringBootApplication
 public class EmployeeApplication {
@@ -10,4 +18,22 @@ public class EmployeeApplication {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
+}
+
+@RestController
+class EmployeeController{
+
+	@GetMapping("/employees")
+	public List<Employee> getEmployees(){
+		List<Employee> employees = List.of(new Employee(1, "Employee A"));
+		return employees;
+	}
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Employee{
+	private Integer id;
+	private String name; 
 }
